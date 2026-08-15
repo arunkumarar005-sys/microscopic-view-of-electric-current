d36d10e1bc2be80d657edc0f4c4de975a5eeb50b
Arunkumar A <arunkumarar005@gmail.com>
2026-08-15 10:57:12 +0530

    Add files via upload

---
+++ story_content/triggers.js
@@ -0,0 +1,72 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6Guhyw67zIH":
+        Script1();
+        break;
+      case "5i0qkrPrx5R":
+        Script2();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var once = player.once;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+var update = player.update;
+var pointerX = player.pointerX;
+var pointerY = player.pointerY;
+var showPointer = player.showPointer;
+var hidePointer = player.hidePointer;
+var slideWidth = player.slideWidth;
+var slideHeight = player.slideHeight;
+var getKeyDown = player.getKeyDown;
+var keydown = player.keydown;
+var keyup = player.keyup;
+window.Script1 = function()
+{
+  player.once(() => {
+const target = object('65QzMMudcbu');
+const duration = 1000;
+const easing = 'ease-out';
+const id = '5pD36TlXS6H';
+const growAmount = 0.3;
+const delay = 1208;
+addToTimeline(
+target.animate(
+[ {scale: `${1 + growAmount}` } ]
+,
+  { fill: 'forwards', delay, duration, easing }
+), id
+);
+});
+}
+
+window.Script2 = function()
+{
+  const target = object('5b71xRHycu9');
+const duration = 500;
+const easing = 'ease-out';
+const id = '6pH9kKcoA3L';
+const pulseAmount = 0.07;
+player.addForTriggers(
+id,
+target.animate(
+[ {scale: '1' }, 
+{scale: `${1 + pulseAmount}` }, 
+{scale: '1' }, 
+{scale: `${1 + pulseAmount}` }, 
+{scale: '1' } ]
+,
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+};
